5966dca04c1d56b801bd9663bedbc773baee1aa2
Tianchen Lan <577189479@qq.com>
2018-09-27 21:36:47 +0800

    add Recommend and Weekend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <img class="item-img" :src="item.imgURL">
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-btn">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgURL: 'http://img1.qunarzz.com/sight/p0/201401/06/3fbbddda915cb7637439d484440d36be.jpg_200x200_a54e87dd.jpg',
+        title: '杭州云曼温泉',
+        desc: '潮流电音、浪漫灯光、梦幻水舞百秀集结'
+      }, {
+        id: '0002',
+        imgURL: 'http://img1.qunarzz.com/sight/p0/1508/12/4e6170303c63bdb7070fb1987480afab.water.jpg_200x200_0775a71c.jpg',
+        title: '杭州极地海洋世界',
+        desc: '潮流电音、浪漫灯光、梦幻水舞百秀集结'
+      }, {
+        id: '0003',
+        imgURL: 'http://img1.qunarzz.com/sight/p0/1808/d6/d6c567a31d0a5462a3.img.jpg_200x200_fe329041.jpg',
+        title: '千岛湖九龙溪漂流',
+        desc: '潮流电音、浪漫灯光、梦幻水舞百秀集结'
+      }, {
+        id: '0004',
+        imgURL: 'http://img1.qunarzz.com/sight/p0/1603/ae/aecef86d7655873f90.water.jpg_200x200_22a0790e.jpg',
+        title: '杭州野生动物世界',
+        desc: '潮流电音、浪漫灯光、梦幻水舞百秀集结'
+      }, {
+        id: '0005',
+        imgURL: 'http://img1.qunarzz.com/sight/p0/1603/18/18ae748f826c5abe90.water.jpg_200x200_67503119.jpg',
+        title: '大明山',
+        desc: '潮流电音、浪漫灯光、梦幻水舞百秀集结'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    background #eee
+    line-height .8rem
+    text-indent .3rem
+    margin-top .1rem
+  .item
+    display flex
+    height 1.9rem
+    overflow hidden
+    .item-img
+      height 1.7rem
+      width 1.7rem
+      padding .1rem
+    .item-info
+      flex 1
+      padding .1rem
+      min-width 0
+      .item-title
+        line-height .54rem
+        font-size .32rem
+        ellipsis()
+      .item-desc
+        line-height .4rem
+        color #ccc
+        ellipsis()
+      .item-btn
+        margin-top .2rem
+        border-radius .1rem
+        background-color #ff9300
+        padding 0 .1rem
+        color #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="title">周末去哪儿</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgURL">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgURL: 'http://img1.qunarzz.com/sight/source/1505/4e/3e87ee62e59cdb.jpg_r_640x214_fe6cc902.jpg',
+        title: '杭州必游TOP10',
+        desc: '杭州，前世今生与你有一场美丽约会'
+      }, {
+        id: '0002',
+        imgURL: 'http://img1.qunarzz.com/sight/source/1505/9f/7d45cc38259e0.jpg_r_640x214_156f4299.jpg',
+        title: '承载杭城的历史之地',
+        desc: '饱经沧桑的地方诉述着这座城的段段衷肠'
+      }, {
+        id: '0003',
+        imgURL: 'http://img1.qunarzz.com/sight/source/1505/ae/4fe23c6b6679e2.jpg_r_640x214_368a18d8.jpg',
+        title: '美仑美奂的演出',
+        desc: '一座有故事的城，天天有“戏”在上演'
+      }, {
+        id: '0004',
+        imgURL: 'http://img1.qunarzz.com/sight/source/1505/c1/1178c5696fcf57.jpg_r_640x214_a69542c6.jpg',
+        title: '杭州江南古镇',
+        desc: '古镇岂只有新叶？悠闲出游杭州古镇'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    background #eee
+    line-height .8rem
+    text-indent .3rem
+    margin-top .1rem
+  .item-img-wrapper
+    overflow hidden
+    height 0
+    padding-bottom 33.9%
+    .item-img
+      width 100%
+  .item-info
+    padding .1rem
+    .item-title
+      line-height .54rem
+      font-size .32rem
+      ellipsis()
+    .item-desc
+      line-height .4rem
+      color #ccc
+      ellipsis()
+</style>
