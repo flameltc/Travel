bd448177a18a78146b61edc4d4aba63848759a18
Tianchen Lan <577189479@qq.com>
2018-09-30 21:39:02 +0800

    search done

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,12 +1,61 @@
 <template>
-  <div class="search">
-    <input type="text" class="input-search" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input v-model="keyword" type="text" class="input-search" placeholder="输入城市名或拼音">
+    </div>
+    <div class="search-content" v-show="keyword" ref="search">
+      <ul>
+        <li v-for="item of list" :key="item.id" class="search-item border-bottom">{{item.name}}</li>
+        <li class="search-item border-bottom" v-show="hasNoData">没有找到地点</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
+import Bscoll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      list: [],
+      timer: null
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  watch: {
+    keyword () {
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  },
+  mounted () {
+    this.scoll = new Bscoll(this.$refs.search)
+  }
 }
 </script>
 
@@ -25,4 +74,18 @@ export default {
       border-radius .06rem
       color #666
       padding 0 .1rem
+  .search-content
+    z-index 1
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+    background #eeeeee
+    .search-item
+      line-height .62rem
+      padding-left  .2rem
+      color #666
+      background-color #fff
 </style>
